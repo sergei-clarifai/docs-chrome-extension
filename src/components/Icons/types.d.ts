5e597bd2d56ea922c4058203814025b35a281f73
Manpreet Gulati <manpreet.gulati@clarifai.com>
2022-05-10 21:55:17 +0300

    add settings icon to settings-button

---
+++ src/components/Icons/types.d.ts
@@ -0,0 +1,4 @@
+export interface IconProps extends React.HTMLAttributes<HTMLDivElement> {
+  color?: string;
+  size?: number;
+}
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Settings } from '../../components/Icons/Settings';
 import Logo from '../../assets/img/clarifai-logo.jpeg';
 import '../../assets/styles/tailwind.css';
 
@@ -18,8 +19,9 @@ export const Navbar = () => {
         <a href="/options.html">
           <button
             type="button"
-            className="py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+            className="flex py-2.5 px-3 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
           >
+            <Settings className='mr-1' color='darkgrey' />
             Configure Settings
           </button>
         </a>
