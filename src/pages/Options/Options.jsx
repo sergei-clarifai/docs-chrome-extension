4e4f4f0261a8a515d28b50b3f8b5c0de253ce5b8
Manpreet Gulati <manpreet.gulati@clarifai.com>
2022-05-11 12:24:48 +0300

    minor styling fix

--- src/pages/Options/Options.jsx
+++ src/pages/Options/Options.jsx
@@ -89,7 +89,7 @@ const Options = ({ title }) => {
                         required />
                     </div>
 
-                    <button id="saveBtn" type="submit" className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Save Token</button>
+                    <button id="saveBtn" type="submit" className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Save Token</button>
                 </form>
               </div>
             </section>
@@ -102,7 +102,7 @@ const Options = ({ title }) => {
                       <input type="text" id="spaceNameInput" name="confluenceSpaceName" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" required />
                     </div>
 
-                    <button id="saveBtn" type="submit" className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Save</button>
+                    <button id="saveBtn" type="submit" className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Save</button>
                 </form>
               </div>
             </section>
